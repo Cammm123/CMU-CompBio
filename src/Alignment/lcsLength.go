5450587b5eadac53521fb3fb28973d67f927b02e
Cammm123 <109167468+Cammm123@users.noreply.github.com>
2024-06-26 08:22:46 -0400

    Adding in alignment

---
+++ src/Alignment/lcsLength.go
@@ -0,0 +1,7 @@
+package main
+
+//LCSLength takes two strings as input. It returns the length of a longest common
+//subsequence of the two strings.
+func LCSLength(str1, str2 string) int {
+	return 0
+}

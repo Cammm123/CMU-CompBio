fa4fa24d5d1bdca4a40a9578a0d8a064acb56b82
Cammm123 <109167468+Cammm123@users.noreply.github.com>
2024-06-30 11:33:20 -0400

    diffusion matrix

--- src/Alignment/main.go
+++ src/Alignment/main.go
@@ -6,4 +6,25 @@ import (
 
 func main() {
 	fmt.Println("Sequence alignment!")
+	Hemoglobin()
+}
+
+func Hemoglobin() {
+	zebrafish := ReadFASTAFile("Data/Hemoglobin/Danio_rerio_hemoglobin.fasta")
+	cow := ReadFASTAFile("Data/Hemoglobin/Bos_taurus_hemoglobin.fasta")
+	gorilla := ReadFASTAFile("Data/Hemoglobin/Gorilla_gorilla_hemoglobin.fasta")
+	human := ReadFASTAFile("Data/Hemoglobin/Homo_sapiens_hemoglobin.fasta")
+
+	match := 1.0
+	mismatch := 1.0
+	gap := 3.0 //insertion or deletion of an amino acid is more deadly in terms of mutations
+
+	alignment1 := GlobalAlignment(zebrafish, human, match, mismatch, gap)
+	WriteAlignmentToFASTA(alignment1, "Output/fish-human.txt")
+
+	alignment2 := GlobalAlignment(cow, human, match, mismatch, gap)
+	WriteAlignmentToFASTA(alignment2, "Output/cow-human.txt")
+
+	alignment3 := GlobalAlignment(gorilla, human, match, mismatch, gap)
+	WriteAlignmentToFASTA(alignment3, "Output/gorilla-human.txt")
 }
--- src/grayScott/functions.go
+++ src/grayScott/functions.go
@@ -1,3 +1,54 @@
 package main
 
-//place your functions from the assignment here.
+import (
+	
+)
+
+func DiffuseBoardOneParticle(currentBoard [][]float64, diffusionRate float64, kernel [3][3]float64) [][]float64 {
+	newBoard := make([][]float64, len(currentBoard))
+	for i := range newBoard {
+		newBoard[i] = make([]float64, len(currentBoard[i]))
+	}
+
+	for row := range currentBoard {
+		for col := range currentBoard[row] {
+			newValue := multiplyOut(currentBoard, kernel, row, col)
+			newBoard[row][col] = newValue * diffusionRate
+		}
+	}
+
+	// Calculate the middle indices
+	midRow := len(newBoard) / 2
+	midCol := len(newBoard[0]) / 2
+
+	// Safely modify the middle cell
+	newBoard[midRow][midCol] = 1 + newBoard[midRow][midCol]
+
+	return newBoard
+}
+
+func multiplyOut(currentBoard [][]float64, kernel [3][3]float64, row, col int) float64 {
+	sum := 0.0
+	kernelSize := len(kernel)
+	offset := kernelSize / 2
+
+	for i := 0; i < kernelSize; i++ {
+		for j := 0; j < kernelSize; j++ {
+			newRow := row + i - offset
+			newCol := col + j - offset
+
+			if InField(currentBoard, newRow, newCol) {
+				sum += currentBoard[newRow][newCol] * kernel[i][j]
+			}
+		}
+	}
+
+	return sum
+}
+
+func InField(currentBoard [][]float64, row, col int) bool {
+	if row < 0 || col < 0 || row >= len(currentBoard) || col >= len(currentBoard[0]) {
+		return false
+	}
+	return true
+}
